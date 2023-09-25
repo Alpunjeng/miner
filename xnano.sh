5a2d908ed112f14ad8afc0052c43366a2d222b74
Alpunjeng <89038608+Alpunjeng@users.noreply.github.com>
2023-09-25 16:44:23 +0700

    xnano.sh

--- xnano.sh
+++ xnano.sh
@@ -1,5 +1,1 @@
-apt get update
-wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz
-tar xmrig-5.11.3-xenial-x64.tar.gz
-cd xmrig-5.11.3
 ./xmrig -a rx -o stratum+ssl://rx-asia.unmineable.com:443 -u XNO:nano_34gecyfxcijn547sm85jb8oocydnyz9b53xkgfum5jia4ztqy6gcbp8dcjxa.cloud -p x 

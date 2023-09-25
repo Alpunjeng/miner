83d70ddc56796afb4c016e6a6cc08f16366978a4
Alpunjeng <89038608+Alpunjeng@users.noreply.github.com>
2023-09-25 10:20:54 +0700

    Create xnano.sh

---
+++ xnano.sh
@@ -0,0 +1,4 @@
+apt get update
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-static-x64.tar.gz
+tar xf xmrig-6.17.0-linux-static-x64.tar.gz
+./xmrig -a rx -o stratum+ssl://rx-asia.unmineable.com:443 -u XNO:nano_34gecyfxcijn547sm85jb8oocydnyz9b53xkgfum5jia4ztqy6gcbp8dcjxa.cloud -p x 
